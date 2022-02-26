101bb7ae3c773f7d1a3dbd6784317bcfd275339a
Pekuru <9923839+Pekuru@users.noreply.github.com>
2022-02-26 13:21:51 +0100

    init

---
+++ markersplus/config.cpp
@@ -0,0 +1,33 @@
+class CfgPatches
+{
+	class TMG_markers
+	{
+		name = "Tactical Mission Graphics";
+		author = "Pek";
+		picture = "";
+		logoSmall = "";
+		logo = "";
+		tooltipOwned = "Tactical Mission Graphics (TMG) By Pek";
+		overview = "Tactical Mission Graphics (TMG) allows platoon/company level Command elements to better manage their markers on the map using different symbols, text and terms.";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		requiredVersion = 0.1;
+	};
+};
+
+class CfgMarkerClasses
+{
+	class TMG_Points
+	{
+		displayName = "[TMG] Points";
+	};
+	class TMG_Actions_Friendly
+	{
+		displayName = "[TMG] Actions by Friendly Forces";
+	};
+	class TMG_Effects_Enemy
+	{
+		displayName = "[TMG] Effects on Enemy Forces";
+	};
+};
